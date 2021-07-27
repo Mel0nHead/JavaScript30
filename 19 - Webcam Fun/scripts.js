c40fc952e3cd5eb2ae9628e130f5ba2e46180292
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2021-07-27 12:21:59 +0100

    finish webcam exercise

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -19,6 +19,24 @@ async function getVideo() {
   }
 }
 
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] += 100;
+    pixels.data[i + 1] -= 50;
+    pixels.data[i + 2] *= 0.5;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 50] = pixels.data[i];
+    pixels.data[i + 50] = pixels.data[i + 1];
+    pixels.data[i - 50] = pixels.data[i + 2];
+  }
+  return pixels;
+}
+
 function paintToCanvas() {
   const { videoHeight, videoWidth } = video;
   canvas.width = videoWidth;
@@ -26,6 +44,9 @@ function paintToCanvas() {
 
   setInterval(() => {
     ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+    let pixels = ctx.getImageData(0, 0, videoWidth, videoHeight);
+    pixels = rgbSplit(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
